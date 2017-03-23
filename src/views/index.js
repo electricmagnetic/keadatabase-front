3ffea711c2137afcab4f379eeb957e8cc17239e6
George Moon <george@catalyst.net.nz>
2017-03-23 16:02:13 +1300

    Added some basic branding and associated colours

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import Blog from '../components/WordPress/Blog';
 import Page from '../components/WordPress/Page';
 import FeaturedBird from '../components/Birds/FeaturedBird';
 import LatestSightings from '../components/Sightings/LatestSightings';
+
+class BannerButtons extends Component {
+  render() {
+    return(
+      <div className="BannerButtons">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6 button-report">
+              <Link to="/report">Report Sightings &raquo;</Link>
+            </div>
+            <div className="col-sm-6 button-birds">
+              <Link to="/birds">Search Birds &raquo;</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class Banner extends Component {
   render() {
@@ -35,6 +55,7 @@ class HomePage extends Component {
     return (
       <div className="HomePage">
         <Banner />
+        <BannerButtons />
         <div className="container">
           <div className="row">
             <div className="col-md-6">
--- src/views/sightings/index.js
+++ src/views/sightings/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import LatestSightings from '../../components/Sightings/LatestSightings';
+
 class SightingsPage extends Component {
   render() {
     return (
       <div className="SightingsPage">
         <div className="container">
           <h1>Sightings List Page</h1>
+          <LatestSightings />
         </div>
       </div>
     );
