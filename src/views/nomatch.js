91497d1120248fe03a570646567b358dffe7ef7f
George Moon <george@moon.org.nz>
2017-04-22 14:58:47 +1200

    added kea to page not found

--- src/views/nomatch.js
+++ src/views/nomatch.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 
+import error from '../assets/img/error.jpg';
+
 class NoMatchPage extends Component {
   render() {
     return (
       <div className="NoMatchPage">
         <Helmet title="Page Not Found" />
-        <div className="container">
-          <h1>Page Not Found</h1>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-md-6 col-md-offset-3">
+              <h1>Page Not Found</h1>
+              <p>
+                <img src={ error } alt="kea on car" className="img-circle" width="128px" />
+              </p>
+              <p className="lead">
+                Hmm, you seem to have come across a page that doesn't exist.
+              </p>
+              <p>
+                If you think there's an issue, please let us know.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
