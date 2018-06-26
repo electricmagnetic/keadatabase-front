62c33cd7d486bb9942d5c4fc3aab0cc53f07f89d
George Moon <george@catalyst.net.nz>
2018-06-26 13:51:53 +1200

    Added DateFormat helper

---
+++ src/components/helpers/FormatDate.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const FormatDate = ({ format, children }) => {
+  var momentFormat = '';
+
+  switch (format) {
+    case 'short':
+      momentFormat = 'DD/MM/YY [at] h:mm a';
+      break;
+    case 'long':
+      momentFormat = 'dddd DD MMMM YYYY [at] h:mm a';
+      break;
+    case 'date':
+      momentFormat = 'ddd DD MMMM';
+      break;
+  default:
+    momentFormat = 'ddd DD/MM/YY [at] h:mm a';
+  }
+
+  return(
+    <Moment format={ momentFormat } parse="YYYY-MM-DD HH:mm:ss">
+      { children }
+    </Moment>
+  );
+};
+
+FormatDate.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.element.isRequired,
+    PropTypes.array.isRequired
+  ]),
+  format: PropTypes.string.isRequired
+}
+
+FormatDate.defaultProps = {
+  format: 'long'
+}
+
+export default FormatDate;
