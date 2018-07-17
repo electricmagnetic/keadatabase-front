304bc03e42e82c2eb19698b5addbdc31f66b7826
Satoshi Sugimura <satoshisugimura@catalyst.net.nz>
2018-07-17 16:37:21 +1200

    Added sightings detail form outline (no actions/reducers yet)

--- src/components/report/sighting/ReportSighting.js
+++ src/components/report/sighting/ReportSighting.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Formik } from 'formik';
 import moment from 'moment';
 
 import SightingDetailsFieldset from './SightingDetailsFieldset';
+// import { getSightingOptions, reportSighting } from '../../../actions/report';
 
-const ReportSighting = () => (
-  <div>
-    <p>All fields are required, except where indicated.</p>
-    <Formik
-      initialValues={{
-        email: '',
-        date: moment(),
-      }}
-      render={props => (
-        <form onSubmit={props.handleSubmit}>
-          <SightingDetailsFieldset {...props} />
-        </form>
-      )}
-    />
-  </div>
-);
+class ReportSighting extends Component {
+  componentDidMount() {
+    // const { getSightingOptions } = this.props;
+    // getSightingOptions();
+  }
 
-export default ReportSighting;
+  render() {
+    // const { sightingOptions, handleSubmit } = this.props;
+    const handleSubmit = null;
+    return (
+      <div>
+        <p>All fields are required, except where indicated.</p>
+        <Formik
+          initialValues={{
+            dateTimeSighted: moment(),
+            longitude: '',
+            latitude: '',
+            locationDetails: '',
+          }}
+          onSubmit={handleSubmit}
+          render={props => (
+            <form onSubmit={props.handleSubmit}>
+              <SightingDetailsFieldset {...props} />
+            </form>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  // sightingOptions: state.reportOptions,
+});
+
+const mapDispatchToProps = dispatch => ({
+  // getSightingOptions: () => { dispatch(getSightingOptions()) },
+  // handleSubmit: values => { dispatch(reportSighting(values)) },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReportSighting);
--- src/components/report/sighting/SightingDetailsFieldset.js
+++ src/components/report/sighting/SightingDetailsFieldset.js
@@ -1,31 +1,101 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import { Marker } from "react-mapbox-gl";
+import { Link } from 'react-router-dom';
 
+import Map from '../../map/Map';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const SightingDetailsFieldset = ({
   values,
-  errors,
-  touched,
   handleChange,
-  handleBlur,
-  handleSubmit,
   setFieldValue,
-  isSubmitting,
 }) => (
   <fieldset>
     <legend>1. Sighting Details</legend>
-    <label>Date and time sighted
+
+    <div className="form-group">
+      <label>Date and time sighted</label>
       <DatePicker
-        selected={values.date}
-        onChange={date => setFieldValue('date', date)}
+        selected={values.dateTimeSighted}
+        onChange={date => setFieldValue('dateTimeSighted', date)}
         showTimeSelect
         timeFormat="HH:mm"
         timeIntervals={15}
         dateFormat="D MMM YYYY, hh:mm"
         timeCaption="time"
       />
-    </label>
+    </div>
+
+    <div className="card">
+      <div className="card-header">
+        Location
+      </div>
+
+      <div className="card-body">
+        <Map
+          onClick={(map, e) => {
+            setFieldValue('longitude', e.lngLat.lng)
+            setFieldValue('latitude', e.lngLat.lat)
+          }}
+        >
+          {values.longitude && values.latitude &&
+            <Marker
+              coordinates={[values.longitude, values.latitude]}
+            >
+              <p>Marker</p>
+            </Marker>
+          }
+        </Map>
+
+        <p>
+          Use the precision dropdown to give us an indication of how accurate the location is (in metres).
+        </p>
+
+        <div className="form-group">
+          <label htmlFor="precision">Precision</label>
+          <select className="form-control" id="precision">
+            <option default value=""></option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            type="text"
+            className="form-control"
+            id="longitude"
+            placeholder="e.g. 171.562"
+            value={values.longitude}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            type="text"
+            className="form-control"
+            id="latitude"
+            placeholder="e.g. -42.940"
+            value={values.latitude}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="locationDetails">Location details (optional)</label>
+          <textarea
+            className="form-control"
+            id="locationDetails"
+            placeholder="e.g. Beside the Arthur's Pass Store"
+            value={values.locationDetails}
+            onChange={handleChange}
+          />
+        </div>
+
+      </div>
+    </div>
   </fieldset>
 );
 
