5128e138240682f942a19be6e504374e5759d18b
Satoshi Sugimura <satoshisugimura@catalyst.net.nz>
2018-07-27 15:40:35 +1200

    Added download csv function

--- src/components/sightings/SightingsCards.js
+++ src/components/sightings/SightingsCards.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import { CSVLink } from 'react-csv';
 
 import FormatDate from '../helpers/FormatDate';
 import Loader from '../helpers/Loader';
@@ -46,9 +47,28 @@ class SightingsCards extends Component {
     else if (sightings.rejected) return <Error reason={ sightings.value.message }/>;
     else if (sightings.fulfilled) {
       const filteredSightings = getFilteredSightings(sightings.value.results, sightingsFilter);
+
+      // csv
+      const csvHeader = [
+        'id', 'get_quality_display', 'contributor', 'get_sighting_type_display',
+        'date_sighted', 'time_sighted', 'region', 'comments', 'quality',
+        'date_created', 'date_updated', 'sighting_type', 'point_location', 'behaviour',
+        'favourite', 'geocode',
+      ];
+      const csvData = filteredSightings.map(sighting => ([
+        sighting.id, sighting.get_quality_display, sighting.contributor,
+        sighting.get_sighting_type_display, sighting.date_sighted,
+        sighting.time_sighted, sighting.region, sighting.comments,
+        sighting.quality, sighting.date_created, sighting.date_updated,
+        sighting.sighting_type, sighting.point_location, sighting.behaviour,
+        sighting.favourite, sighting.geocode
+      ]));
+
       return (
         <div className="SightingsCards">
-          <p>Showing {filteredSightings.length} results</p>
+          <span>Showing {filteredSightings.length} results</span>&nbsp;
+          <CSVLink headers={csvHeader} data={csvData} filename="kea-sightings.csv">Download CSV</CSVLink>
+
           <div className="row">
             { filteredSightings.map((sighting) =>
               <div className="col-sm-4 col-md-3" key={ sighting.id }>
