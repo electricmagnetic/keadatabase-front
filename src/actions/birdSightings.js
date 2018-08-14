2da7d2b7fcdb0e899ac881db05e079eb02f71e11
Satoshi Sugimura <satoshisugimura@catalyst.net.nz>
2018-08-14 16:58:49 +1200

    Implemented sighted birds

--- src/actions/birdSightings.js
+++ src/actions/birdSightings.js
@@ -4,18 +4,19 @@ export const BIRDSIGHTINGS_REQUEST = 'birdSightings/REQUEST';
 export const BIRDSIGHTINGS_RECEIVE = 'birdSightings/RECEIVE';
 export const BIRDSIGHTINGS_ERROR = 'birdSightings/ERROR';
 
-function fetchBirdSightings() {
+function fetchBirdSightings(id) {
+  const query = id ? `?sighting=${id}&has_bird=1` : '?has_bird=2&page_size=5';
   return {
     [RSAA]: {
-      endpoint: `https://api.keadatabase.nz/sightings/birds/?has_bird=2&page_size=5`,
+      endpoint: `https://api.keadatabase.nz/sightings/birds/${query}`,
       method: 'GET',
       headers: { 'Accept': 'application/json' },
       types: [BIRDSIGHTINGS_REQUEST, BIRDSIGHTINGS_RECEIVE, BIRDSIGHTINGS_ERROR]
     }
-  }
+  };
 }
 
-function shouldFetchBirdSightings(state) {
+function shouldFetchBirdSightings(state, id) {
   const { birdSightings } = state;
 
   if (birdSightings.pending) {
@@ -23,16 +24,17 @@ function shouldFetchBirdSightings(state) {
   }
 
   if (birdSightings.fulfilled) {
+    if (id) return true;
     return false;
   }
 
   return true;
 }
 
-export function getBirdSightings() {
+export function getBirdSightings(id = '') {
   return (dispatch, getState) => {
-    if (shouldFetchBirdSightings(getState())) {
-      return dispatch(fetchBirdSightings());
+    if (shouldFetchBirdSightings(getState(), id)) {
+      return dispatch(fetchBirdSightings(id));
     }
-  }
+  };
 }
--- src/components/sightings/SightedBirds.js
+++ src/components/sightings/SightedBirds.js
@@ -3,34 +3,72 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const BirdCard = bird => (
-  <div className='card'>
-    <div className='card-body'>
-      <h2 className="card-title h5"><Link to={ '/birds/' + bird.slug }>{ bird.name }&nbsp;&raquo;</Link></h2>
-      <p className="card-text">
-        { bird.band_combo }
-      </p>
+import { getBirdSightings } from '../../actions/birdSightings';
+import Loader from '../helpers/Loader';
+import Error from '../helpers/Error';
+
+const BirdCard = ({ bird }) => {
+  let name = 'Unknown';
+  if (bird.bird) {
+    name = <Link to={ '/birds/' + bird.bird }>{ bird.get_bird_display }&nbsp;&raquo;</Link>;
+  } else if (bird.get_life_stage_guess_display || bird.get_sex_guess_display) {
+    name = `${bird.get_life_stage_guess_display} ${bird.get_sex_guess_display}`;
+  }
+
+  const bandCombo = bird.band_combo
+    ? bird.band_combo
+    : bird.get_banded_display;
+
+  return (
+    <div className='col-6 col-md-3'>
+      <div className='card'>
+        <div className='card-body'>
+          <h2 className="card-title h5">{ name }</h2>
+          <p className="card-text">
+            { bandCombo }
+          </p>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 class SightedBirds extends Component {
+  componentDidMount() {
+    const { id, dispatch } = this.props;
+    dispatch(getBirdSightings(id));
+  }
+
   render() {
-    return (
-      <div className='container'>
-        <h2>Birds</h2>
-      </div>
-    );
+    const { birdSightings } = this.props;
+
+    if (birdSightings.pending) return <Loader />;
+    else if (birdSightings.rejected) return <Error reason={ birdSightings.value.message }/>;
+    else if (birdSightings.fulfilled) {
+
+      return (
+        <div className='container'>
+          <h2>Birds</h2>
+          <div className='row'>
+            {birdSightings.value.results.map(bird => (
+              <BirdCard bird={ bird } key={ bird.id } />
+            ))}
+          </div>
+        </div>
+      );
+    }
+    else return null;
   }
 }
 
 SightedBirds.propTypes = {
   id: PropTypes.string.isRequired,
+  birdSightings: PropTypes.object,
   dispatch: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-
+  birdSightings: state.birdSightings,
 });
 
 export default connect(mapStateToProps)(SightedBirds);
