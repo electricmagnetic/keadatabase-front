ddeda70f277f9a918998b8514ea8f79d4a3544cb
Satoshi Sugimura <satoshisugimura@catalyst.net.nz>
2018-08-15 10:32:28 +1200

    Added unmatched + refactored

--- src/components/sightings/SightedBirds.js
+++ src/components/sightings/SightedBirds.js
@@ -8,26 +8,30 @@ import Loader from '../helpers/Loader';
 import Error from '../helpers/Error';
 import './SightedBirds.css';
 
-const BirdCard = ({ bird }) => {
-  let name = 'Unknown';
-  if (bird.bird) {
-    name = <Link to={ '/birds/' + bird.bird }>{ bird.get_bird_display }&nbsp;&raquo;</Link>;
-  } else if (bird.get_life_stage_guess_display || bird.get_sex_guess_display) {
-    name = `${bird.get_life_stage_guess_display} ${bird.get_sex_guess_display}`;
+const BirdCard = ({ birdSighting }) => {
+  const matched = birdSighting.bird ? true : false;
+  const known = !matched && (birdSighting.life_stage_guess || birdSighting.sex_guess) ? true : false;
+  const banded = birdSighting.band_combo ? true : false;
+
+  let name = <em>Unknown</em>;
+  if (matched) {
+    name = <Link to={ '/birds/' + birdSighting.bird }>{ birdSighting.get_bird_display }&nbsp;&raquo;</Link>;
+  } else if (known) {
+    name = `${birdSighting.get_life_stage_guess_display} ${birdSighting.get_sex_guess_display}`;
+  } else if (banded) {
+    name = <em>Unmatched</em>;
   }
 
-  const bandCombo = bird.band_combo
-    ? bird.band_combo
-    : <em>{ bird.get_banded_display }</em>;
+  const bandCombo = banded
+    ? birdSighting.band_combo
+    : <em>{ birdSighting.get_banded_display }</em>;
 
   return (
     <div className='col-6 col-md-3'>
       <div className='card'>
         <div className='card-body'>
           <h5 className="card-title">{ name }</h5>
-          <p className="card-text">
-            { bandCombo }
-          </p>
+          <p className="card-text">{ bandCombo }</p>
         </div>
       </div>
     </div>
@@ -54,8 +58,8 @@ class SightedBirds extends Component {
         <div className='SightedBirds container'>
           <h2>Birds</h2>
           <div className='row'>
-            {birdSightings.value.results.map(bird => (
-              <BirdCard bird={ bird } key={ bird.id } />
+            {birdSightings.value.results.map(birdSighting => (
+              <BirdCard birdSighting={ birdSighting } key={ birdSighting.id } />
             ))}
           </div>
         </div>
