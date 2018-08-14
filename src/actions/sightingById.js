0dcd721e982bd26e6cafaf825c2d264684f9b071
Satoshi Sugimura <satoshisugimura@catalyst.net.nz>
2018-08-14 13:42:46 +1200

    Separated sightingById action

---
+++ src/actions/sightingById.js
@@ -0,0 +1,54 @@
+import { RSAA, getJSON } from 'redux-api-middleware';
+
+import { SIGHTINGS_REQUEST, SIGHTINGS_RECEIVE, SIGHTINGS_ERROR } from './sightings';
+
+function fetchSightingById(id) {
+  return {
+    [RSAA]: {
+      endpoint: `https://api.keadatabase.nz/sightings/sightings/${id}`,
+      method: 'GET',
+      headers: { 'Accept': 'application/json' },
+      types: [
+        SIGHTINGS_REQUEST,
+        {
+          type: SIGHTINGS_RECEIVE,
+          // Process payload so that it doesn't break state structure
+          payload: (action, state, res) => {
+            return getJSON(res).then(json => ({
+              count: 1,
+              next: null,
+              previous: null,
+              results: [json]
+            }));
+          },
+        },
+        SIGHTINGS_ERROR
+      ]
+    }
+  };
+}
+
+function shouldFetchSightingById(state, id) {
+  const { sightings } = state;
+
+  if (sightings.pending) {
+    return false;
+  }
+
+  if (sightings.fulfilled) {
+    const found = sightings.value && sightings.value.results
+      && sightings.value.results.find(sighting => sighting.id === Number(id));
+    if (found) return false;
+    return true;
+  }
+
+  return true;
+}
+
+export function getSightingById(id = '') {
+  return (dispatch, getState) => {
+    if (shouldFetchSightingById(getState(), id)) {
+      return dispatch(fetchSightingById(id));
+    }
+  };
+}
--- src/components/sightings/SightingDetail.js
+++ src/components/sightings/SightingDetail.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { getSightings } from '../../actions/sightings';
+import { getSightingById } from '../../actions/sightingById';
 import Loader from '../helpers/Loader';
 import Error from '../helpers/Error';
 
 class SightingDetail extends Component {
   componentDidMount() {
     const { id, dispatch } = this.props;
-    dispatch(getSightings({ id }));
+    dispatch(getSightingById(id));
   }
 
   render() {
