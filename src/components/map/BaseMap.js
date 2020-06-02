e636ddf1b7d14472a5798c8a597bb077c5ed3c62
George Moon <george@moon.org.nz>
2020-06-03 11:08:21 +1200

    Minor fixes to map layout and tiles

--- src/components/map/BaseMap.js
+++ src/components/map/BaseMap.js
@@ -37,7 +37,6 @@ class BaseMap extends Component {
         <TileLayer
           attribution="Mapbox"
           url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_API_KEY}`}
-          maxZoom={10}
         />
         <TileLayer
           url={`https://tiles-{s}.data-cdn.linz.govt.nz/services;key=${process.env.REACT_APP_LINZ_API_KEY}/tiles/v4/layer=50798/EPSG:3857/{z}/{x}/{y}.png`}
