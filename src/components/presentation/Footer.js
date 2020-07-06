e4752462d70eeef8f4d5011d5eb5d838d1cc3d79
George Moon <george@moon.org.nz>
2020-07-06 15:49:11 +1200

    Minor tweaks to layout of sponsors and footer

--- src/components/presentation/Footer.js
+++ src/components/presentation/Footer.js
@@ -7,44 +7,48 @@ const Footer = props => {
   return (
     <footer className="d-print-none">
       <div className="constrainer">
-        <div className="container py-3">
-          <ul className="footer-links mb-3">
-            <li>
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/sponsor">Sponsor A Kea</NavLink>
-            </li>
-            <li>
-              <NavLink to="/help">Help</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/licence">Licence/Copyright</NavLink>
-            </li>
-            <li>
-              <NavLink to="/terms">Terms/Privacy</NavLink>
-            </li>
-            <li>
-              <a href="https://blog.keadatabase.nz">Blog</a>
-            </li>
-          </ul>
-          <p className="footer-attribution">
-            Kea Database data hosted in New Zealand on{' '}
-            <a
-              href="https://www.catalyst.net.nz/products/gis-core"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="catalyst"
-            >
-              Catalyst GIS Core
-            </a>
-            .
-          </p>
+        <div className="container my-4">
+          <nav>
+            <ul className="footer-links my-2">
+              <li>
+                <NavLink exact to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/sponsor">Sponsor Kea</NavLink>
+              </li>
+              <li>
+                <NavLink to="/help">Help</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/licence">Licence/Copyright</NavLink>
+              </li>
+              <li>
+                <NavLink to="/terms">Terms/Privacy</NavLink>
+              </li>
+              <li>
+                <a href="https://blog.keadatabase.nz">Blog</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="footer-attribution">
+            <p>
+              Kea Database data hosted in New Zealand on{' '}
+              <a
+                href="https://www.catalyst.net.nz/products/gis-core"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="catalyst"
+              >
+                Catalyst GIS Core
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </footer>
