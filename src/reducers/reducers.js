8dc4c0de1e3ec3da70589dbae99abc06a805ffcd
George Moon <george@moon.org.nz>
2020-09-15 12:44:44 +1200

    Removing old reference

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import bandCombos from './bandCombos';
 import { reportObservationOptions, reportObservationPost } from './reportObservation';
 
 export default combineReducers({
-  bandCombos,
   reportObservationOptions,
   reportObservationPost,
   router: routerReducer,
