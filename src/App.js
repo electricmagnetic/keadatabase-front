8d9950aab182538fc16692fe5d160ddaac248ade
George Moon <george@moon.org.nz>
2021-01-12 19:36:22 +1300

    Updating SWR config and version

--- src/App.js
+++ src/App.js
@@ -32,6 +32,19 @@ import ReportObservationSuccessPage from './views/report/success';
 import NoMatchPage from './views/nomatch';
 
 const CACHE_TIME = 24 * 60 * 60 * 1000;
+const fetcher = async url => {
+  const result = await fetch(url);
+
+  if (!result.ok) {
+    const error = new Error('An error occurred while fetching the data.');
+
+    error.info = await result.json();
+    error.status = result.status;
+    throw error;
+  }
+  return result.json();
+};
+
 const store = configureStore();
 
 initGa(history);
@@ -41,7 +54,7 @@ class App extends Component {
     return (
       <SWRConfig
         value={{
-          fetcher: (...args) => fetch(...args).then(result => result.json()),
+          fetcher: fetcher,
           dedupingInterval: CACHE_TIME,
           revalidateOnFocus: false,
         }}
